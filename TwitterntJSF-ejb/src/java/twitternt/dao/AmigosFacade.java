2d25fc536ec454c606d13934c05f3d2ae596a911
marcoasg <trigitos@uma.es>
2019-05-23 12:20:54 +0200

    funciona lista de amigos y borrar amigo

--- TwitterntJSF-ejb/src/java/twitternt/dao/AmigosFacade.java
+++ TwitterntJSF-ejb/src/java/twitternt/dao/AmigosFacade.java
@@ -5,10 +5,12 @@
  */
 package twitternt.dao;
 
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import twitternt.entity.Amigos;
+import twitternt.entity.Usuario;
 
 /**
  *
@@ -29,4 +31,58 @@ public class AmigosFacade extends AbstractFacade<Amigos> {
         super(Amigos.class);
     }
     
+        private List<Usuario> findByUser1(Integer userId){
+        return em.createQuery("SELECT a.usuario FROM Amigos a WHERE a.usuario1.id = :userId AND a.solicitudAceptada = TRUE")
+                .setParameter("userId", userId)
+                .getResultList();
+    }
+    
+    private List<Usuario> findByUser2(Integer userId){
+        return em.createQuery("SELECT a.usuario1 FROM Amigos a WHERE a.usuario.id = :userId AND a.solicitudAceptada = TRUE")
+                .setParameter("userId", userId)
+                .getResultList();
+    }
+    
+    public List<Usuario> findByUser(Integer userId){
+        List<Usuario> lista = findByUser1(userId);
+        List<Usuario> lista1 = findByUser2(userId);
+        if (lista1.size() >= 1)
+            lista.addAll(findByUser2(userId));
+        return lista;
+    }
+    
+    public List<Usuario> findPetitionsByUser(Integer userId){
+        return em.createQuery("SELECT a.usuario FROM Amigos a WHERE a.usuario1.id = :userId AND a.solicitudAceptada = FALSE")
+                .setParameter("userId", userId)
+                .getResultList();
+    }
+
+    public Amigos findFriendByPair(Integer codigoAmigo, Integer userId) {
+        try {
+            return (Amigos) em.createNamedQuery("Amigos.findFriendByPair")
+                    .setParameter("amigo1", codigoAmigo)
+                    .setParameter("amigo2", userId)
+                    .getSingleResult();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    
+    public Amigos findPetitionByPair(Integer codigoAmigo, Integer userId) {
+        try {
+            return (Amigos) em.createNamedQuery("Amigos.findPetitionByPair")
+                    .setParameter("amigo1", codigoAmigo)
+                    .setParameter("amigo2", userId)
+                    .getSingleResult();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    
+    public boolean sendPetitionAvailable(Integer u1, Integer u2){
+        Amigos a1 = findPetitionByPair(u2, u1);
+        Amigos a2 = findFriendByPair(u2, u1);
+        
+        return a1 == null && a2 == null;
+    }
 }
---
+++ TwitterntJSF-war/src/java/twitternt/bean/AmigosBean.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package twitternt.bean;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.inject.Inject;
+import twitternt.dao.AmigosFacade;
+import twitternt.entity.Usuario;
+
+/**
+ *
+ * @author Trigi
+ */
+@Named(value = "amigosBean")
+@SessionScoped
+public class AmigosBean implements Serializable {
+
+    @EJB
+    private AmigosFacade amigosFacade;
+
+    @Inject
+    protected LoginBean loginBean;
+    
+    private List<Usuario> listaAmigos;
+    
+    private Usuario seleccionado;
+
+    /**
+     * Creates a new instance of AmigosBean
+     */
+    public AmigosBean() {
+    }
+    
+    @PostConstruct
+    public void init(){
+        listaAmigos = amigosFacade.findByUser(loginBean.getUserId());
+    }
+    
+    public String doEliminar(){
+        this.amigosFacade.remove(amigosFacade.findFriendByPair(seleccionado.getId(), loginBean.getUserId()));
+        init();
+        return "amigos";
+    }
+    
+    public List<Usuario> getListaAmigos(){
+        return listaAmigos;
+    }
+    
+    public void setListaAmigos(List<Usuario> listaAmigos){
+        this.listaAmigos = listaAmigos;
+    }
+
+    public Usuario getSeleccionado() {
+        return seleccionado;
+    }
+
+    public void setSeleccionado(Usuario seleccionado) {
+        this.seleccionado = seleccionado;
+    }
+    
+    
+}
